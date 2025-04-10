082ff93ea3eb70d1dd905e7c0f10d82c974ba94a
09295091612 <09295091612@192.168.56.1>
2025-04-10 08:27:49 -0300

    arquivos

--- src/javarecordscontrol/FormSistema.java
+++ src/javarecordscontrol/FormSistema.java
@@ -1,13 +1,16 @@
 
 package javarecordscontrol;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import javax.swing.JOptionPane;
-import javax.swing.JTextArea;
+
 public class FormSistema extends javax.swing.JFrame {
     // Declarar estruturas
+    
+    private Pilha<Recorde> pilhaRecorde = new Pilha<>(8);
+    private final Pilha<Recorde> pilha2 = new Pilha<>(8);
     
     public FormSistema() {
         initComponents();
@@ -32,30 +35,31 @@ public class FormSistema extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         txtTempo = new javax.swing.JTextField();
         txtData = new javax.swing.JFormattedTextField();
+        btnLimpa = new javax.swing.JButton();
+        btnSalva = new javax.swing.JButton();
+        btnCarrega = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
 
         jPanel1.setBackground(new java.awt.Color(0, 0, 0));
 
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/javarecordscontrol/corrida.png"))); // NOI18N
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/javarecordscontrol/relogioresize.png"))); // NOI18N
         jLabel2.setToolTipText("");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+            .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 283, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 283, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 269, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         jPanel2.setBackground(new java.awt.Color(255, 255, 255));
@@ -96,7 +100,7 @@ public class FormSistema extends javax.swing.JFrame {
         });
 
         jLabel3.setFont(new java.awt.Font("Segoe UI Black", 0, 36)); // NOI18N
-        jLabel3.setText("Registrador de recordes");
+        jLabel3.setText("Controle de Recordes");
 
         txtTempo.setFont(new java.awt.Font("Segoe UI Light", 0, 18)); // NOI18N
         txtTempo.setBorder(javax.swing.BorderFactory.createTitledBorder("Tempo"));
@@ -108,6 +112,36 @@ public class FormSistema extends javax.swing.JFrame {
             ex.printStackTrace();
         }
 
+        btnLimpa.setFont(new java.awt.Font("Monospaced", 0, 18)); // NOI18N
+        btnLimpa.setText("Limpar");
+        btnLimpa.setActionCommand("Remover");
+        btnLimpa.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        btnLimpa.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLimpaActionPerformed(evt);
+            }
+        });
+
+        btnSalva.setFont(new java.awt.Font("Monospaced", 0, 18)); // NOI18N
+        btnSalva.setText("Salvar");
+        btnSalva.setActionCommand("Remover");
+        btnSalva.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        btnSalva.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSalvaActionPerformed(evt);
+            }
+        });
+
+        btnCarrega.setFont(new java.awt.Font("Monospaced", 0, 18)); // NOI18N
+        btnCarrega.setText("Carregar");
+        btnCarrega.setActionCommand("Remover");
+        btnCarrega.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        btnCarrega.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCarregaActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
@@ -115,20 +149,31 @@ public class FormSistema extends javax.swing.JFrame {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(51, 51, 51)
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
                         .addGap(64, 64, 64)
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(lblTopo)
                             .addGroup(jPanel2Layout.createSequentialGroup()
                                 .addComponent(txtNome, javax.swing.GroupLayout.PREFERRED_SIZE, 236, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(45, 45, 45)
-                                .addComponent(txtData, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 47, Short.MAX_VALUE)
+                                .addComponent(txtData, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(55, 55, 55)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addGap(39, 39, 39)
+                                .addComponent(btnCarrega, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(btnLimpa, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(btnSalva, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addGap(26, 26, 26)))
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 326, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtTempo, javax.swing.GroupLayout.PREFERRED_SIZE, 235, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtTempo, javax.swing.GroupLayout.PREFERRED_SIZE, 235, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 326, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(26, 26, 26))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                 .addGap(143, 143, 143)
@@ -148,21 +193,27 @@ public class FormSistema extends javax.swing.JFrame {
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(lblTopo)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(txtData)
                     .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(txtNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addComponent(txtTempo, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(14, 14, 14)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnRemove, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnAdd, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 329, Short.MAX_VALUE)
-                    .addComponent(jScrollPane1))
-                .addContainerGap())
+                .addGap(50, 50, 50)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 329, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 329, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(btnLimpa, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnSalva, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(28, 28, 28)
+                        .addComponent(btnCarrega, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(31, 31, 31))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -170,9 +221,8 @@ public class FormSistema extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(10, 10, 10)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(22, 22, 22)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
@@ -186,20 +236,117 @@ public class FormSistema extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+    
+    private void atualizarTela() {
+        
+        listPilha.setText(""); 
+        listAux.setText("");   
 
+        for (int i = pilhaRecorde.size() - 1; i >= 0; i--) {
+            Recorde r = (Recorde) pilhaRecorde.get(i);
+            listPilha.append(r.toString() + "\n");
+        }
 
+        for (int i = pilha2.size() - 1; i >= 0; i--) {
+            Recorde r = (Recorde) pilha2.get(i);
+            listAux.append(r.toString() + "\n");
+        }
+
+        // Atualiza o texto do topo
+        if (!pilhaRecorde.isEmpty()) {
+            lblTopo.setText("Topo: " + pilhaRecorde.peek().getNome());
+        } else {
+            lblTopo.setText("Topo: -");
+        }
+    }
+
+   
     
     private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddActionPerformed
-       
-       
-
+        //Pega os valores do nome tempo e data 
+        String nome = txtNome.getText().trim();
+        String tempoTexto = txtTempo.getText().trim();
+        String dataTexto =txtData.getText().trim();
+        
+        //Ve se nao esta vazio
+        if(nome.isEmpty() || tempoTexto.isEmpty() || dataTexto.isEmpty()){
+            //Abre a uma "Janelinha" que exibe a informação Preencha o campo 
+            JOptionPane.showMessageDialog(this,"Preencha todos os campos");
+            return;
+        }
+        
+        //Verifica se o tempo e um numero valido 
+        //matches e um metodo para verificar os padroes de texto
+        if(!tempoTexto.matches("\\d+(\\.\\d+)?")){
+            JOptionPane.showMessageDialog(this, "Tempo inválido! Use somente números (ex: 9.58)");
+            return;
+        }
+        
+        //Agora validar o formato da Data
+        if(!dataTexto.matches("\\d{2}/\\d{2}/\\d{4}")){
+            JOptionPane.showMessageDialog(this, "Data inválida! Use o formato dd/MM/aaaa");
+            return;
+        }
+        
+        double tempo = Double.parseDouble(tempoTexto);  
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dataRecorde = LocalDate.parse(dataTexto, formatter);
+        
+        //cria novo Recorde
+        Recorde novo = new Recorde();
+        novo.setNome(nome);
+        novo.setTempo(tempo);
+        novo.setDataRecorde(dataRecorde);
+        
+        if(pilhaRecorde.isEmpty() || tempo<pilhaRecorde.peek().getTempo()){
+            pilhaRecorde.push(novo);
+            atualizarTela();
+        }else{
+            JOptionPane.showMessageDialog(this,"Tempo deve ser menor que o recorde atual!");
+        }
     }//GEN-LAST:event_btnAddActionPerformed
 
     private void btnRemoveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRemoveActionPerformed
+        if(pilhaRecorde.isEmpty()){
+            JOptionPane.showMessageDialog(this,"A pilha principal está vazia");
+            return;
+        }
         
+        Recorde removido = pilhaRecorde.pop();
+        
+        pilha2.push(removido);
+        
+        atualizarTela();
 
         
     }//GEN-LAST:event_btnRemoveActionPerformed
+
+    private void btnLimpaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLimpaActionPerformed
+        pilhaRecorde.clear();
+        pilha2.clear();
+        atualizarTela();
+    }//GEN-LAST:event_btnLimpaActionPerformed
+//peguei da internet nao sabia salvar nem carregar ai tive que improvisar ;-;
+    private void btnSalvaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalvaActionPerformed
+        try {
+        PilhaSalvar.salvarPilha(pilhaRecorde, "recordes.txt");
+        JOptionPane.showMessageDialog(this, "Recordes salvos com sucesso!");
+        }   
+        catch (IOException e) {
+        JOptionPane.showMessageDialog(this, "Erro ao salvar: " + e.getMessage());
+        }
+    }//GEN-LAST:event_btnSalvaActionPerformed
+
+    private void btnCarregaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCarregaActionPerformed
+        try {
+        pilhaRecorde = PilhaSalvar.carregarPilha("recordes.txt", 8);
+        atualizarTela();
+        JOptionPane.showMessageDialog(this, "Recordes carregados com sucesso!");
+        } 
+        catch (IOException e) {
+        JOptionPane.showMessageDialog(this, "Erro ao carregar: " + e.getMessage());
+        }
+    }//GEN-LAST:event_btnCarregaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -229,16 +376,17 @@ public class FormSistema extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new FormSistema().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new FormSistema().setVisible(true);
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAdd;
+    private javax.swing.JButton btnCarrega;
+    private javax.swing.JButton btnLimpa;
     private javax.swing.JButton btnRemove;
+    private javax.swing.JButton btnSalva;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
--- src/javarecordscontrol/Pilha.java
+++ src/javarecordscontrol/Pilha.java
@@ -63,5 +63,9 @@ public class Pilha<T> {
     Object get(int i) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+
+    void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
