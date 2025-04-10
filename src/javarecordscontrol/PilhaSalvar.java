e1334a997af5162dea00dc362639ce828b4d6a3c
nicol <nicol@DESKTOP-TUP66JU>
2025-04-09 21:03:33 -0300

    arquivos

---
+++ src/javarecordscontrol/PilhaSalvar.java
@@ -0,0 +1,44 @@
+package javarecordscontrol;
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class PilhaSalvar {
+ public static <T> void salvarPilha(Pilha<T> pilha, String caminho) throws IOException {
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(caminho))) {
+        for (int i = 0; i < pilha.size(); i++) {
+            writer.write(pilha.get(i).toString());
+            writer.newLine();
+        }
+    }
+}
+
+
+    public static Pilha<Recorde> carregarPilha(String caminho, int tamanhoMaximo) throws IOException {
+        Pilha<Recorde> pilha = new Pilha<>(tamanhoMaximo);
+        BufferedReader reader = new BufferedReader(new FileReader(caminho));
+        String linha;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        while ((linha = reader.readLine()) != null) {
+            if (linha.startsWith("Nome:")) {
+                // Divide a linha por espaço
+                String[] partes = linha.split(" ");
+
+                String nome = partes[0].substring("Nome:".length());
+                double tempo = Double.parseDouble(partes[1].substring("Tempo:".length()));
+                LocalDate data = LocalDate.parse(partes[2].substring("Data:".length()), formatter);
+
+                Recorde recorde = new Recorde();
+                recorde.setNome(nome);
+                recorde.setTempo(tempo);
+                recorde.setDataRecorde(data);
+
+                pilha.push(recorde);
+            }
+        }
+
+        reader.close();
+        return pilha;
+    }
+}
